9f8e39ca6ab9a86700ff3771f8fec37c758ce746
Marc UHART <marcuhart@gmail.com>
2023-03-22 13:59:40 -0700

    Adding GraphQL

--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -1,15 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using ConferencePlanner.GraphQL.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ConferencePlanner.GraphQL
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
-            => services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlite("Data Source=conferences.db"));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
+        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
         }
     }
 }
