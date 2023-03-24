a9ebd462b62d1472313a385f6f0ff58e112e9d48
Marc UHART <marcuhart@gmail.com>
2023-03-24 11:17:36 -0700

    Offer plural versions fields and be precise about field names

--- GraphQL/Speakers/SpeakerQueries.cs
+++ GraphQL/Speakers/SpeakerQueries.cs
@@ -13,10 +13,16 @@ namespace ConferencePlanner.GraphQL
         public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) =>
             context.Speakers.ToListAsync();
 
-        public Task<Speaker> GetSpeakerAsync(
-            [ID(nameof(Speaker))] int id,
+        public Task<Speaker> GetSpeakerByIdAsync(
+            [ID(nameof(Speaker))]int id,
             SpeakerByIdDataLoader dataLoader,
             CancellationToken cancellationToken) =>
             dataLoader.LoadAsync(id, cancellationToken);
+
+        public async Task<IEnumerable<Speaker>> GetSpeakersByIdAsync(
+            [ID(nameof(Speaker))]int[] ids,
+            SpeakerByIdDataLoader dataLoader,
+            CancellationToken cancellationToken) =>
+            await dataLoader.LoadAsync(ids, cancellationToken);
     }
 }
